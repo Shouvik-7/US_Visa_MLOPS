93cb4a896132a89b0ca702b36a8e32dfaadb7330
Shouvik <shouvik1999@gmail.com>
2024-11-09 15:11:10 -0600

    Add logging logic

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shouvik",
+    author_email="shouvik1999@gmail.com",
+    packages=find_packages()
+)
